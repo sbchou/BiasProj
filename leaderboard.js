f4b1a2ca5d76e54d1a64641b604088c769ef1711
Cat <Cat@catbook.media.mit.edu>
2016-02-26 15:24:43 -0500

    wonky login page but works!

--- leaderboard.js
+++ leaderboard.js
@@ -339,6 +339,164 @@ if (Meteor.isClient) {
    }
   });
 
+/**
+  Template._loginButtonsLoggedInDropdown.events({
+    'click #login-buttons-edit-profile': function(event) {
+      Router.go('profileEdit');
+    }
+  });
+
+  Accounts.ui.config({
+      requestPermissions: {},
+      extraSignupFields: [{
+          fieldName: 'first-name',
+          fieldLabel: 'First name',
+          inputType: 'text',
+          visible: true,
+          validate: function(value, errorFunction) {
+            if (!value) {
+              errorFunction("Please write your first name");
+              return false;
+            } else {
+              return true;
+            }
+          }
+      }, {
+          fieldName: 'last-name',
+          fieldLabel: 'Last name',
+          inputType: 'text',
+          visible: true,
+      }, {
+          fieldName: 'gender',
+          showFieldLabel: true,      // If true, fieldLabel will be shown before radio group
+          fieldLabel: 'Gender',
+          inputType: 'radio',
+          radioLayout: 'vertical',    // It can be 'inline' or 'vertical'
+          data: [{                    // Array of radio options, all properties are required
+          id: 1,                  // id suffix of the radio element
+              label: 'Male',          // label for the radio element
+              value: 'm'              // value of the radio element, this will be saved.
+            }, 
+            {
+              id: 2,
+              label: 'Female',
+              value: 'f',
+              checked: 'checked'
+            },
+            {
+              id:3,
+              label: 'Not Listed',
+              value: 'o',
+              checked: 'checked'
+            }
+          ],
+          visible: true
+      }, {
+          fieldName: 'country',
+          fieldLabel: 'Country',
+          inputType: 'select',
+          showFieldLabel: true,
+          empty: 'Please select your country of residence',
+          data: [{
+              id: 1,
+              label: 'United States',
+              value: 'us'
+            }, {
+              id: 2,
+              label: 'Canada',
+              value: 'ca'
+            }, {
+              id: 3,
+              label: 'Other',
+              value: 'o'
+            }
+          ],
+          visible: true
+      }, {
+          fieldName: 'terms',
+          fieldLabel: 'I accept the terms and conditions',
+          inputType: 'checkbox',
+          visible: true,
+          saveToProfile: false,
+          validate: function(value, errorFunction) {
+              if (value) {
+                  return true;
+              } else {
+                  errorFunction('You must accept the terms and conditions.');
+                  return false;
+              }
+          }
+      }]
+  });
+
+**/
+
+  //Use in  place of Meteor.logout() in your client code.  Also called automatically by atNavButton when clicking Sign Off
+  AccountsTemplates.logout();
+
+  var myPostLogout = function(){
+      //example redirect after logout
+      Router.go('/home');
+  };
+
+
+  AccountsTemplates.configure({
+      // Behavior
+      confirmPassword: true,
+      enablePasswordChange: true,
+      forbidClientAccountCreation: false,
+      overrideLoginErrors: true,
+      sendVerificationEmail: false,
+      lowercaseUsername: false,
+      focusFirstInput: true,
+
+      // Appearance
+      showAddRemoveServices: false,
+      showForgotPasswordLink: false,
+      showLabels: true,
+      showPlaceholders: true,
+      showResendVerificationEmailLink: false,
+
+      // Client-side Validation
+      continuousValidation: false,
+      negativeFeedback: false,
+      negativeValidation: true,
+      positiveValidation: true,
+      positiveFeedback: true,
+      showValidating: true,
+
+      // Privacy Policy and Terms of Use
+      privacyUrl: 'privacy',
+      termsUrl: 'terms-of-use',
+
+      // Redirects
+      homeRoutePath: '/home',
+      redirectTimeout: 4000,
+
+      // Hooks
+      onLogoutHook: myPostLogout,
+      /**
+      onLogoutHook: myLogoutFunc,
+      onSubmitHook: mySubmitFunc,
+      preSignUpHook: myPreSubmitFunc,
+      postSignUpHook: myPostSubmitFunc,
+      **/
+
+      // Texts
+      texts: {
+        button: {
+            signUp: "Register Now!"
+        },
+        socialSignUp: "Register",
+        socialIcons: {
+            "meteor-developer": "fa fa-rocket"
+        },
+        title: {
+            forgotPwd: "Recover Your Password"
+        },
+      },
+  });
+
 
  }
 
